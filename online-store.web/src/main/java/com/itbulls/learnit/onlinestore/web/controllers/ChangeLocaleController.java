6542ffd2c3778826feb25455bd4d891df30d210f
m-troja <m-troja@github.com>
2024-10-20 18:56:19 +0200

    Webapp is launching

--- online-store.web/src/main/java/com/itbulls/learnit/onlinestore/web/controllers/ChangeLocaleController.java
+++ online-store.web/src/main/java/com/itbulls/learnit/onlinestore/web/controllers/ChangeLocaleController.java
@@ -1,18 +1,18 @@
 package com.itbulls.learnit.onlinestore.web.controllers;
 
-import jakarta.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.util.Locale;
 
 import com.itbulls.learnit.onlinestore.web.Configurations;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/change-locale")
-public class ChangeLocaleServlet extends HttpServlet {
+public class ChangeLocaleController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String language = request.getParameter("locale");
 		if (language == null) {
--- online-store.web/src/main/java/com/itbulls/learnit/onlinestore/web/controllers/NotFoundErrorHandlerController.java
+++ online-store.web/src/main/java/com/itbulls/learnit/onlinestore/web/controllers/NotFoundErrorHandlerController.java
@@ -1,17 +1,17 @@
 package com.itbulls.learnit.onlinestore.web.controllers;
 
-import jakarta.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 
 import com.itbulls.learnit.onlinestore.web.Configurations;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/not-found")
-public class NotFoundErrorHandlerServlet extends HttpServlet {
+public class NotFoundErrorHandlerController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher(Configurations.VIEWS_PATH_RESOLVER 
